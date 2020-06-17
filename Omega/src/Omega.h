72f8aef5dff5778b5ded7aa336424c8728aa2945
moddedTechnic <jpleeming51@gmail.com>
2020-06-17 10:42:57 +0100

    Added logging

--- Omega/src/Omega.h
+++ Omega/src/Omega.h
@@ -3,6 +3,7 @@
 // For use by Omega applications
 
 #include "Omega/Application.h"
+#include "Omega/Log.h"
 
 // Entry Point
 #include "Omega/EntryPoint.h"
--- Omega/src/Omega/EntryPoint.h
+++ Omega/src/Omega/EntryPoint.h
@@ -5,6 +5,10 @@
 extern Omega::Application* Omega::CreateApplication();
 
 int main(int arg, char** argv) {
+	Omega::Log::Init();
+	OM_CORE_INFO("Initialised Log");
+	OM_INFO("Initialised Log");
+
 	auto app = Omega::CreateApplication();
 	app->Run();
 	delete app;
